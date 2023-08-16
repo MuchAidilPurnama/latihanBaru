61a87ba485ec1e58d7eb2297ec27d437e7109fea
putrinovitaa14 <putrinovitasari01411@gmail.com>
2023-08-16 14:40:15 +0700

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -37,7 +37,7 @@
               <li><a class="text-sm text-gray-400 hover:text-gray-500" href="/contact">Contact</a></li>
           </ul>
 
-          <!-- <div class="hidden xl:flex items-right text-gray-600 space-x-10 items-right">
+          <div class="hidden xl:flex items-center text-gray-600 space-x-10 items-center mr-9">
                     <a class="flex items-center hover:text-gray-900" href="#">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -47,14 +47,23 @@
                             <span class="h-3 w-3 relative inline-flex rounded-full bg-pink-600"></span>
                         </span>
                     </a>
-                </div> -->
+                </div>
           
       <div v-if="isAuthenticated">
-        <button @click="logout" class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-300 hover:bg-gray-200 rounded-xl">Logout</button>
+        <button @click="logout" class="relative inline-block px-4 py-2 font-medium group">
+<span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+<span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+<span class="relative text-black group-hover:text-white">Logout</span>
+</button>
         </div>
         <div v-else>
-          <a href="/login" class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-300 hover:bg-gray-200 rounded-xl">Login</a>
+            <a href="/login" class="relative inline-block px-4 py-2 font-medium group">
+<span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+<span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+<span class="relative text-black group-hover:text-white">Login</span>
+</a>
         </div>
+        
           
       </nav>
       <div class="navbar-menu relative z-50 hidden">
